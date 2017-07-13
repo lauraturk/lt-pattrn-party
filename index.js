ca1cc769dc45a7dc689fd9c9e73de68d5cbeb32c
Laura Turk <laura.lk.turk@gmail.com>
2017-07-12 21:39:05 -0600

    accordian functionality

---
+++ index.js
@@ -0,0 +1,18 @@
+const openTab = (event, tabName ) => {
+    const tabFill = document.getElementsByClassName("tab-fill");
+
+    Object.keys(tabFill).forEach((tab) => {
+      tabFill[tab].style.display = "none";
+    })
+
+    const tabLink = document.getElementsByClassName("tab-link");
+
+    Object.keys(tabLink).forEach((link) => {
+      tabLink[link].className = tabLink[link].className.replace(" active", "");
+    })
+    
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+}
+
+  document.getElementById("defaultOpen").click();
